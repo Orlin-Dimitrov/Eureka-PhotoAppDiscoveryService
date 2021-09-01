879d48ea08f16ab0c00cf522ec96253b9945f4d2
Orlin Dimitrov <orlin.dimitrov@mail.bg>
2021-09-01 10:51:43 +0300

    first commit

---
+++ src/test/java/com/or2d/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.or2d.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
